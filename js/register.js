c7caa2568cb1c43e8410454b05fa3ab9f2267654
Samuel O. Asare <asare11samuel@gmail.com>
2022-11-19 02:10:56 +0000

    initial commit

--- js/register.js
+++ js/register.js
@@ -4,9 +4,17 @@ const lastName = document.querySelector("#lastName")
 const email = document.querySelector("#email")
 const password = document.querySelector("#password")
 
+function validateForm(){
+    return firstName.value && lastName.value && email.value && password.value
+}
 
 form.addEventListener("submit", (e)=>{
     e.preventDefault()
+
+    if(!validateForm()){
+        alert("Form validation failed")
+        return;
+    }
 
     alert(`Hi, ${firstName.value} ${lastName.value}. You have been successfully registered.`)
     form.reset()
